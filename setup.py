bbacb4374a22e71bb4b877269d51ea8f73d7f75a
Mukund Iyer <msiyer@student.ubc.ca>
2024-02-07 16:15:01 -0800

    docker and aws set up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="yolo5-detection",
+    version = "0.0.0",
+    author = "Mukund"
+    packages = find_package(),
+    install_requires = []
+
+)
